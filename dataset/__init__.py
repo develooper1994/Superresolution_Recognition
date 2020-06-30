dfa1d40837d988bcb93b0a7a9fd22a3c15ec3f6c
Mustafa Selçuk Çağlar <selcukcaglar08@gmail.com>
2020-06-30 16:43:57 +0300

    I have just started

---
+++ dataset/__init__.py
@@ -0,0 +1,12 @@
+try:
+    from .Captcha import *
+    from .FAKE_dataset import *
+    from .IAM_dataset import *
+    from .superresolution_dataset import *
+    from .UFPR_ALPR import *
+except:
+    from Captcha import *
+    from FAKE_dataset import *
+    from IAM_dataset import *
+    from superresolution_dataset import *
+    from UFPR_ALPR import *
---
+++ dataset/superresolution_dataset/superresolution_dataset.py
@@ -0,0 +1,56 @@
+import glob
+
+import numpy as np
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+from torch.utils.data import Dataset
+
+# Normalization parameters for pre-trained PyTorch models
+mean = np.array([0.485, 0.456, 0.406])
+std = np.array([0.229, 0.224, 0.225])
+
+
+def denormalize(tensors):
+    """ Denormalizes image tensors using mean and std """
+    for c in range(3):
+        tensors[:, c].mul_(std[c]).add_(mean[c])
+    return torch.clamp(tensors, 0, 255)
+
+
+class ImageDataset_superresolution(Dataset):
+    def __init__(self, root=None, hr_shape=None):
+        hr_height, hr_width = hr_shape
+        # Transforms for low resolution images and high resolution images
+        self.lr_transform = transforms.Compose(
+            [
+                transforms.Resize((hr_height // 4, hr_height // 4), Image.BICUBIC),
+                transforms.ToTensor(),
+                transforms.Normalize(mean, std),
+            ]
+        )
+        self.hr_transform = transforms.Compose(
+            [
+                transforms.Resize((hr_height, hr_height), Image.BICUBIC),
+                transforms.ToTensor(),
+                transforms.Normalize(mean, std),
+            ]
+        )
+
+        if isinstance(root, str):
+            self.files = sorted(glob.glob(root + "/*.*"))
+        else:
+            self.files = root
+
+    def __getitem__(self, index):
+        if isinstance(self.files, str):
+            img = Image.open(self.files[index % len(self.files)])
+        else:
+            img = self.files
+        img_lr = self.lr_transform(img)
+        img_hr = self.hr_transform(img)
+
+        return {"lr": img_lr, "hr": img_hr}
+
+    def __len__(self):
+        return len(self.files)
